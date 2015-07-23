8b96df746cc00fc4ca1f3a2c7fe7ea13ea0e8f0d
Alexey ALexandrov <aleksandrov1988@gmail.com>
2015-07-23 18:01:48 +0300

    Multirow overfull realization

--- lib/rails-latex/latex_to_pdf.rb
+++ lib/rails-latex/latex_to_pdf.rb
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 class LatexToPdf
   def self.config
-    @config||={:command => 'pdflatex', :arguments => ['-halt-on-error'], :parse_twice => false, :parse_runs => 1}
+    @config||={:command => 'pdflatex', :arguments => ['-halt-on-error'], :parse_twice => false, :parse_runs => 1,
+               :overfull_vbox => true}
   end
 
   # Converts a string of LaTeX +code+ into a binary string of PDF.
@@ -12,47 +13,74 @@ class LatexToPdf
   # The config argument defaults to LatexToPdf.config but can be overridden using @latex_config.
   #
   # The parse_twice argument and using config[:parse_twice] is deprecated in favor of using config[:parse_runs] instead.
-  def self.generate_pdf(code,config,parse_twice=nil)
+  def self.generate_pdf(code, config, parse_twice=nil)
     config=self.config.merge(config)
     parse_twice=config[:parse_twice] if parse_twice.nil? # deprecated
     parse_runs=[config[:parse_runs], (parse_twice ? 2 : config[:parse_runs])].max
     puts "Running Latex #{parse_runs} times..."
-    dir=File.join(Rails.root,'tmp','rails-latex',"#{Process.pid}-#{Thread.current.hash}")
-    input=File.join(dir,'input.tex')
+    dir=File.join(Rails.root, 'tmp', 'rails-latex', "#{Process.pid}-#{Thread.current.hash}")
+    input_filename=input.tex
+    input=File.join(dir, input_filename)
     FileUtils.mkdir_p(dir)
     # copy any additional supporting files (.cls, .sty, ...)
     supporting = config[:supporting]
     if supporting.kind_of?(String) or supporting.kind_of?(Pathname) or (supporting.kind_of?(Array) and supporting.length > 0)
       FileUtils.cp_r(supporting, dir)
     end
-    File.open(input,'wb') {|io| io.write(code) }
+    File.open(input, 'wb') { |io| io.write(code) }
     Process.waitpid(
-      fork do
-        begin
-          Dir.chdir dir
-          original_stdout, original_stderr = $stdout, $stderr
-          $stderr = $stdout = File.open("input.log","a")
-          args=config[:arguments] + %w[-shell-escape -interaction batchmode input.tex]
-          (parse_runs-1).times do
-            system config[:command],'-draftmode',*args
+        fork do
+          begin
+            Dir.chdir dir
+            if config[:overfull_vbox]
+              cmd="#{config[:command]} #{input_filename} | grep 'Overfull \\\\vbox'"
+              res=`#{cmd}`.split("\n")
+              overfulls = []
+              res.each do |line|
+                line =~ /\((.*?)pt.*?([0-9]+)\z/
+                overfulls << [$2.to_i - 1, $1.to_f.ceil]
+              end
+              buf = File.readlines(input_filename)
+              val=0
+              File.open(input_filename, 'w') do |f|
+                buf.each_with_index do |line, index|
+                  if overfulls.size > 0 && index == overfulls.first.first
+                    new_val=overfulls.shift.last
+                    val = new_val if new_val > val
+                  end
+                  if val > 0 && line =~ /\\\\/
+                    f.puts line.sub(/\\\\[^\[]/, "\\\\\\[#{val}pt]")
+                    val=0
+                  else
+                    f.puts line
+                  end
+                end
+              end
+            end
+
+            original_stdout, original_stderr = $stdout, $stderr
+            $stderr = $stdout = File.open("input.log", "a")
+            args=config[:arguments] + %w[-shell-escape -interaction batchmode input.tex]
+            (parse_runs-1).times do
+              system config[:command], '-draftmode', *args
+            end
+            exec config[:command], *args
+          rescue
+            File.open("input.log", 'a') { |io|
+              io.write("#{$!.message}:\n#{$!.backtrace.join("\n")}\n")
+            }
+          ensure
+            $stdout, $stderr = original_stdout, original_stderr
+            Process.exit! 1
           end
-          exec config[:command],*args
-        rescue
-          File.open("input.log",'a') {|io|
-            io.write("#{$!.message}:\n#{$!.backtrace.join("\n")}\n")
-          }
-        ensure
-          $stdout, $stderr = original_stdout, original_stderr
-          Process.exit! 1
-        end
-      end)
-    if File.exist?(pdf_file=input.sub(/\.tex$/,'.pdf'))
-      FileUtils.mv(input,File.join(dir,'..','input.tex'))
-      FileUtils.mv(input.sub(/\.tex$/,'.log'),File.join(dir,'..','input.log'))
+        end)
+    if File.exist?(pdf_file=input.sub(/\.tex$/, '.pdf'))
+      FileUtils.mv(input, File.join(dir, '..', 'input.tex'))
+      FileUtils.mv(input.sub(/\.tex$/, '.log'), File.join(dir, '..', 'input.log'))
       result=File.read(pdf_file)
       FileUtils.rm_rf(dir)
     else
-      raise "pdflatex failed: See #{input.sub(/\.tex$/,'.log')} for details"
+      raise "pdflatex failed: See #{input.sub(/\.tex$/, '.log')} for details"
     end
     result
   end
@@ -71,16 +99,16 @@ class LatexToPdf
         class << (@latex_escaper=Object.new)
           ESCAPE_RE=/([{}_$&%#])|([\\^~|<>])/
           ESC_MAP={
-            '\\' => 'backslash',
-            '^' => 'asciicircum',
-            '~' => 'asciitilde',
-            '|' => 'bar',
-            '<' => 'less',
-            '>' => 'greater',
+              '\\' => 'backslash',
+              '^' => 'asciicircum',
+              '~' => 'asciitilde',
+              '|' => 'bar',
+              '<' => 'less',
+              '>' => 'greater',
           }
 
-          def latex_esc(text)   # :nodoc:
-            text.gsub(ESCAPE_RE) {|m|
+          def latex_esc(text) # :nodoc:
+            text.gsub(ESCAPE_RE) { |m|
               if $1
                 "\\#{m}"
               else
